94a2b58d0907c54319fe856c521ca77e3ffadbcc
Andréas Forsbom <andreasforsbom@gmail.com>
2017-08-20 14:41:47 +0200

    Remove node_modules from repo

---
+++ www/addons/learnwithlara.libs.default/specification.js
@@ -0,0 +1,9 @@
+var specification = {
+	"Name": "Default Accepted Libs",
+	"AddonIdentifier": "learnwithlara.libs.default",
+	"Author": "Various",
+	"Version": "0.0.1",
+	"SupportedCores": ["0.0.1"],
+	"AddonType": "LibPackage",
+	"Dependencies": []
+}
---
+++ www/addons/learnwithlara.skins.default/specification.js
@@ -0,0 +1,10 @@
+var specification = {
+	"Name": "Default Skin",
+	"AddonIdentifier": "learnwithlara.skins.default",
+	"Author": "Learn with Lara",
+	"Version": "0.0.1",
+	"SupportedCores": ["0.0.1"],
+	"AddonType": "Skin",
+	"Dependencies": []
+}
+
