1424b84ef64343424c47b8b2bd2f7364de02c55c
Andréas Forsbom <andreasforsbom@gmail.com>
2018-03-04 17:40:33 +0100

    Changed the way main addon is initialized

--- www/core/core.js
+++ www/core/core.js
@@ -1,5 +1,7 @@
 var identifiedAddons = []
 var addons = []
+var addonSpecs = [];
+
 var activeWidget = null;
 
 function isPhone() {
@@ -24,6 +26,12 @@ class Core {
 			if(addon.MainView != undefined) {
 				addon.MainView = file.fullPath.replace("/www/", "").replace("specification.json", "") + addon.MainView;
 			}
+
+			if(addon.Views != undefined) {
+				$.each(addon.Views, (i, view) => {
+					addon.Views[i] = file.replace("/www/", "").replace("specification.json", "") + addon.Views[i];
+				});
+			}
 		}  else {
 			if(addon.MainClassFile != undefined) {
 				addon.MainClassFile = file.replace("http://localhost:8000/", "").replace("specification.json", "") + addon.MainClassFile;
@@ -31,6 +39,13 @@ class Core {
 
 			if(addon.MainView != undefined) {
 				addon.MainView = file.replace("http://localhost:8000/", "").replace("specification.json", "") + addon.MainView;
+			}
+
+			if(addon.Views != undefined) {
+				$.each(addon.Views, (i, view) => {
+					addon.Views[i].file = file.replace("http://localhost:8000/", "").replace("specification.json", "") + addon.Views[i].file;
+					console.log(view);
+				});
 			}
 		}
 		
@@ -46,16 +61,20 @@ class Core {
 		for(let addon of Core.getIdentifiedAddons()) {
 			if(addon.AddonType) {
 				$('head').append('<script type=text/javascript" src="'+addon.MainClassFile+'"></script>');
-				addons[addon.MainClass] = Function("param", "return new "+addon.MainClass+"(param)");
-				
-				// Instantiate addon class //
-				var adn = new addons[addon.MainClass](addon);
+				addons[addon.AddonIdentifier] = Function("param", "return new "+addon.MainClass+"(param)");
+				addonSpecs[addon.AddonIdentifier] = addon;
 
-				if(adn.addonType == "Main" && activeWidget == null) {
-					activeWidget = adn;
+				if(addon.AddonType == "Skin" || addon.AddonType == "Sounds") {
+					// Here we should add the resources to a list
+				}
+
+				if(addon.AddonType == "Main" && activeWidget == null) {
+
+					// Instantiate addon class //
+					activeWidget = new addons[addon.AddonIdentifier](addon);
 
 					// Start main addon //
-					adn.start();
+					activeWidget.start();
 				}
 			}			
 		}
