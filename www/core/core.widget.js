932223532c19d899f42c9c172b96fe814dd9c1f6
Andréas Forsbom <andreasforsbom@gmail.com>
2018-03-15 20:54:55 +0100

    Add scripts to head aswell

--- www/core/core.widget.js
+++ www/core/core.widget.js
@@ -1,9 +1,13 @@
 class Widget extends Addon {
 	constructor (specification) {
 		super (specification);
+
+		this.path = "/addons/"+specification.AddonIdentifier;
 	}
 
 	start() {
+
+		activeWidget = this;
 		/** Shall become more sophisticated and shouldn't be an $ ajax call, 
 		 * which means we get more control over the content, 
 		 * we could even define our own kind of view format.*/
@@ -29,6 +33,13 @@ class Widget extends Addon {
 					var href = link.outerHTML.replace('href="', 'href="/addons/'+identifier.toLowerCase()+'/');
 					$('head').append(href);
 				});
+
+				_.map(jqView.filter("script"), (script) => {					
+					var src = script.outerHTML.replace('src="', 'src="/addons/'+identifier.toLowerCase()+'/');
+					$('head').append(src);
+				});
+
+
 
 				//append view body
 				$('body').html(view);
