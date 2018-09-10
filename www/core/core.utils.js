8bc5aa15ecb4a9d6697e6b3b47ddcec90d6d094c
Andréas Forsbom <andreasforsbom@gmail.com>
2018-09-11 00:49:40 +0200

    Changed viewport meta tag and added util method to make a widget zommable

--- www/core/core.utils.js
+++ www/core/core.utils.js
@@ -16,4 +16,9 @@ class CoreUtils {
             && /^file:\/{3}[^\/]/i.test(window.location.href)
             && /ios|iphone|ipod|ipad|android/i.test(navigator.userAgent);
     }
+
+    makeZoomable() {
+        $('meta[name=viewport]').remove();
+		$('head').append('<meta name="viewport" content="width=device-width, initial-scale=yes">' );
+    }
 }
