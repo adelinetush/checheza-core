8958714da31f09170544dbc2c65a8db88d58ed99
Andréas Forsbom <andreasforsbom@gmail.com>
2018-11-02 10:35:32 +0100

    Added smoother fading between widgets

--- www/core/core.widget.js
+++ www/core/core.widget.js
@@ -10,7 +10,6 @@ class Widget extends Addon {
 		// get head from view and append to active head
 		Widget.changeView(this.identifier, this.mainView)
 			.then(() => {
-				$('body').removeAttr("style");
 				var scale = 'scale(1)';
 				document.body.style.webkitTransform = scale;     // Chrome, Opera, Safari
 				document.body.style.msTransform = scale;       // IE 9
@@ -21,6 +20,8 @@ class Widget extends Addon {
 				$('head').append('<meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">' );
 
 				this.initialize();
+
+				$('#core_app_container').fadeIn(500);				
 			});
 	}
 
@@ -29,24 +30,31 @@ class Widget extends Addon {
 		return new Promise((resolve, reject) => {
 			core.filesystem.readFile(viewurl)
 				.then(view => {
-					var jqView = $(view);
+					$('#core_app_container').fadeOut(250, () => {
+						// Delete any previous resources to free up memory
+						$("head").find('.resource').remove();
+						//append view body
+						$('#core_app_container').html("");
+						var jqView = $(view);
+						let v = /<body.*?>([\s\S]*)<\/body>/.exec(view)[1];
+						$('#core_app_container').html(v);
 
-					// make sure full paths for resources are added to head.
-					_.map(jqView.filter("link"), (link) => {
-						var href = link.outerHTML.replace('href="', 'class="' + identifier.toLowerCase() + ' resource" href="addons/' + identifier.toLowerCase() + '/');
-						$('head').append(href);
+						// make sure full paths for resources are added to head.
+						_.map(jqView.filter("link"), (link) => {
+							var href = link.outerHTML.replace('href="', 'class="' + identifier.toLowerCase() + ' resource" href="addons/' + identifier.toLowerCase() + '/');
+							$('head').append(href);
+						});
+						
+						_.map(jqView.filter("script"), (script) => {
+							var src = script.outerHTML.replace('src="', 'class="' + identifier.toLowerCase() + ' resource" src="addons/' + identifier.toLowerCase() + '/');
+							$('head').append(src);
+						});
+						
+						resolve(v);
+
+
 					});
 
-					_.map(jqView.filter("script"), (script) => {
-						var src = script.outerHTML.replace('src="', 'class="' + identifier.toLowerCase() + ' resource" src="addons/' + identifier.toLowerCase() + '/');
-						$('head').append(src);
-					});
-
-					let v = /<body.*?>([\s\S]*)<\/body>/.exec(view)[1];
-
-					//append view body
-					$('body').html(v);
-					resolve(v)
 				}).catch(err => {
 					reject(err);
 				});
